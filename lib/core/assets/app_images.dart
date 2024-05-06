a79bd316679263b3977f0f3a2ef218775fd4eaa8
Alex Martynov <martynov.alex@gmail.com>
2024-05-06 13:08:32 +0600

    feat: add images for introduction screen

--- lib/core/assets/app_images.dart
+++ lib/core/assets/app_images.dart
@@ -1,5 +1,19 @@
 class AppImages {
   AppImages._();
 
-  static const List<String> values = [];
+  static const String introductionScreenBuilding =
+      'assets/images/introduction_screen_building.png';
+  static const String introductionScreenDesign =
+      'assets/images/introduction_screen_design.png';
+  static const String introductionScreenDesigner =
+      'assets/images/introduction_screen_designer.png';
+  static const String introductionScreenLayout =
+      'assets/images/introduction_screen_layout.png';
+
+  static const List<String> values = [
+    introductionScreenBuilding,
+    introductionScreenDesign,
+    introductionScreenDesigner,
+    introductionScreenLayout
+  ];
 }
--- lib/core/assets/app_vectors.dart
+++ lib/core/assets/app_vectors.dart
@@ -1,5 +1,19 @@
 class AppVectors {
   AppVectors._();
 
-  static const List<String> values = [];
+  static const String introductionScreenBuilding =
+      'assets/vectors/introduction_screen_building.svg';
+  static const String introductionScreenDesign =
+      'assets/vectors/introduction_screen_design.svg';
+  static const String introductionScreenDesigner =
+      'assets/vectors/introduction_screen_designer.svg';
+  static const String introductionScreenLayout =
+      'assets/vectors/introduction_screen_layout.svg';
+
+  static const List<String> values = [
+    introductionScreenBuilding,
+    introductionScreenDesign,
+    introductionScreenDesigner,
+    introductionScreenLayout
+  ];
 }
